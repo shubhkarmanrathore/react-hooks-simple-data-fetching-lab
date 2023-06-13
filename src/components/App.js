e5e6b0fdae4573f951a073b86920e7526ca8f7bb
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-06-12 19:18:57 -0700

    adds date fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchDog = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        setImage(data.message);
+        setLoaded(true);
+      } catch (error) {
+        console.error("Error fetching dog image:", error);
+      }
+    };
+
+    fetchDog();
+  }, []);
+
+  if (!loaded) return <h3>Loading...</h3>;
+  
+  return (
+    <>
+      <img src={image} alt="A Random Dog" />
+    </>
+  );
+}
+
+export default App;
